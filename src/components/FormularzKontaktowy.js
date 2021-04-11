b77aa9609b0efa013792bde9817d239637fb8dca
grzegorzglabpl <grzegorz.glab@glab.pl>
2021-04-11 10:07:08 +0200

    1 file modified, 1 added

--- src/components/FormularzKontaktowy.js
+++ src/components/FormularzKontaktowy.js
@@ -1,12 +1,29 @@
 import React from "react";
+import "./FormularzKontaktowy.css";
 
 const FormularzKontaktowy = () => {
+
   return (
-    <form action="#form-kontakt-1" method="get">
+    <form id="formularzKontaktowy" className="formularzKontaktowy">
       <h1>Formularz kontaktowy</h1>
       <fieldset>
         <legend>Dane kontaktowe</legend>
-        <input name="parametr1" type="text" value="wartości" />
+        <label for="imie">Podaj imie:</label>
+        <input
+          id="imie"
+          type="text"
+          name="Imie"
+          required
+          minlength="4"
+          maxlength="20"
+        />
+
+        <label for="nazwisko">Podaj nazwisko:</label>
+        <input id="nazwisko" type="text" name="Nazwisko" />
+        <label for="email">Podaj email:</label>
+        <input id="email" type="email" name="Email" required />
+
+        <div className="walidator">Niepoprawna wartosc dla pola imie</div>
       </fieldset>
       <fieldset>
         <legend>Zgody marketingowe</legend>
@@ -18,7 +35,7 @@ const FormularzKontaktowy = () => {
         <button
           type="submit"
           formmethod="get"
-          formaction="#form-kontakt-action-1"
+          formaction="#form-kontakt-action-2"
         >
           Wyślij 1
         </button>
